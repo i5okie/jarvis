19adf8f75802c57b284a2b5e07a7194951be3005
ivan P <ipzent@gmail.com>
2017-02-02 16:55:31 -0800

    stylizing

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,17 +1,17 @@
 module ItemsHelper
 	def weight_kg(n)
-		if n
-			Unit.new(n).to_s("kg")
-		else
+		if n.nil? || n.empty?
 			"kilograms"
+		else
+			Unit.new(n).to_s("kg")
 		end
 	end
 	
 	def weight_lbs(n)
-		if n
-			Unit.new(n).to_s(:lbs)
-		else
+		if n.nil? || n.empty?
 			"pounds"
+		else
+			Unit.new(n).to_s(:lbs)
 		end
 	end
 end
